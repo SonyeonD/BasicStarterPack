35eb469bb25310c25d6f77f88d1f367a5623be3f
Ja_Hwang <dmagk560@gmail.com>
2023-06-20 00:22:26 +0900

    Add Training1

---
+++ Training1/Training1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Training1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
